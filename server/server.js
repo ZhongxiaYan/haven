413dc2789a6a4c379ac64ec2fa026e1f03c97982
Zhongxia Yan <zxyan@berkeley.edu>
2018-09-15 01:55:37 -0400

    added express backend code with mongodb

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const port = process.env.PORT || 5555;
+
+let users = null;
+MongoClient.connect('mongodb://localhost:27017/db', { useNewUrlParser: true }, (err, db) => {
+    if (err) {
+        throw err;
+    }
+    const databaseName = 'yc';
+    let database = db.db(databaseName);
+    console.log(`Created database ${databaseName}`);
+    users = database.collection('users');
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+      res.json({success: true});
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
