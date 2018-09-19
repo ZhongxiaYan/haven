2a6773da8da74a67b108932d83e68cab3a68089c
Zhongxia Yan <zxyan@berkeley.edu>
2018-09-19 01:46:57 -0400

    Connect to server's database

--- dev_server.sh
+++ dev_server.sh
@@ -1,1 +1,1 @@
-ssh -i key.pem -nN -R localhost:5555:localhost:5555 hackhouse@35.233.215.12
+ssh -i key.pem -nN -R localhost:5555:localhost:5555 -L localhost:27017:localhost:27017 hackhouse@35.233.215.12
