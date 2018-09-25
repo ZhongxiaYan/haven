27bd38974314b0358026a05bc8074f514219bee3
Zhongxia Yan <zxyan@berkeley.edu>
2018-09-25 18:58:00 -0400

    added google map component

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 import Context from '../Context';
 
@@ -9,9 +10,11 @@ export default class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            propertyList: []
+            propertyList: [],
+            hoveredProperty: null
         };
         this.getPropertyList = this.getPropertyList.bind(this);
+        this.setHoveredProperty = this.setHoveredProperty.bind(this);
     }
 
     componentDidMount() {
@@ -29,17 +32,26 @@ export default class HomePage extends Component {
         });
     }
 
+    setHoveredProperty(isHover, propertyId) {
+        if (isHover) {
+            this.setState({ hoveredProperty: propertyId });
+        } else {
+            if (propertyId === this.state.hoveredProperty) {
+                this.setState({ hoveredProperty: null });
+            }
+        }
+    }
+
     render() {
-        let { propertyList } = this.state;
+        let { propertyList, hoveredProperty } = this.state;
         return (
             <Context.Consumer>
-                {({ history, setModalState }) => (
+                {({ setModalState }) => (
                     <div id="home-main" className="color-background">
                         <div id="home-property-list">
-                            {propertyList.map((data, i) => <PropertyCard key={i} data={data} setModalState={setModalState} history={this.props.history} />)}
+                            {propertyList.map((data, i) => <PropertyCard key={i} data={data} setModalState={setModalState} history={this.props.history} setHoveredProperty={this.setHoveredProperty} />)}
                         </div>
-                        <div id="home-property-map">
-                        </div>
+                        <GoogleMapComponent propertyList={propertyList} hoveredProperty={hoveredProperty} />
                     </div>
                 )}
             </Context.Consumer>
@@ -49,10 +61,10 @@ export default class HomePage extends Component {
 
 class PropertyCard extends Component {
     render() {
-        let { history, data } = this.props;
+        let { history, data, setHoveredProperty } = this.props;
         let { _id, numBedrooms, numBathrooms, area, formattedAddress, openHouse, rent } = data;
         return (
-            <div className="home-card" onClick={() => (history.push(`/renter/${_id}`))}>
+            <div className="home-card" onClick={() => (history.push(`/renter/${_id}`))} onMouseEnter={() => setHoveredProperty(true, _id)} onMouseLeave={() => setHoveredProperty(false, _id)}>
                 <img className="home-card-image-big" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" />
                 <div className="home-card-container-image">
                     <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" />
@@ -67,6 +79,38 @@ class PropertyCard extends Component {
                     </Panel.Body>
                 </Panel>
             </div>
+        );
+    }
+}
+
+const MapComponent = withScriptjs(withGoogleMap(({ defaultCenter, propertyList, hoveredProperty }) =>
+    <GoogleMap defaultZoom={15} defaultCenter={defaultCenter}>
+        {propertyList.map(property => <Marker key={property._id} position={property.location} opacity={property._id === hoveredProperty ? 1 : 0.5} />)}
+    </GoogleMap>
+));
+
+class GoogleMapComponent extends Component {
+    render() {
+        let { propertyList, hoveredProperty } = this.props;
+        if (propertyList.length === 0) {
+            return null;
+        }
+        let locs = propertyList.map(prop => prop.location);
+        let defaultCenter = {
+            lat: locs.map(loc => loc.lat).reduce((a, b) => a + b, 0) / locs.length,
+            lng: locs.map(loc => loc.lng).reduce((a, b) => a + b, 0) / locs.length
+        }
+
+        return (
+            <MapComponent
+                defaultCenter={defaultCenter}
+                hoveredProperty={hoveredProperty}
+                propertyList={propertyList}
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4DTyzogm6CRc2oZMne4yb75SWyelvavE&v=3.exp&libraries=geometry,drawing,places"
+                containerElement={<div id="home-maps-container-element" />}
+                loadingElement={<div id="home-maps-loading-element" />}
+                mapElement={<div id="home-maps-map-element" />}
+            />
         );
     }
 }
--- preview.js
+++ preview.js
@@ -2,9 +2,16 @@ const routes = require('express').Router();
 
 const Property = require('./models/property');
 
+function maskAttributes(property) {
+    let { address, numBedrooms, numBathrooms, rent, openHouse } = property;
+    let { neighborhood, locality, administrativeAreaLevel1, postalCode } = address;
+    let location = { neighborhood, locality, administrativeAreaLevel1, postalCode };
+    return { location, numBedrooms, numBathrooms, rent, openHouse };
+}
+
 routes.get('/property_list', (req, res) => {
     Property.find().exec().then(properties => {
-        res.json(properties.slice(0, 3));
+        res.json(properties.slice(0, 3).map(maskAttributes));
     });
 });
 
